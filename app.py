8cc3f3b2fa3f08fa835d66d3fa5d90128947cbd3
Vlad Silin <vsilin@uwaterloo.ca>
2017-01-12 12:14:47 -0500

    RB-3. Initialize project for Heroku

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import json
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
